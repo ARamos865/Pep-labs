0e59d1c264316a1200441e6daa76d20a98fe6d70
Ashley Ramos <ashley.ramos865@my.tccd.edu>
2022-12-24 04:30:12 +0000

    Saved progress on MethodParameters 2022-12-24 04:30:12 +0000

--- MethodParameters/src/main/java/Parrot.java
+++ MethodParameters/src/main/java/Parrot.java
@@ -12,6 +12,10 @@ public class Parrot {
      * @return text.
      */
     public String copy(String text){
-        return "return text please!";
+        return text;
+    }
+
+    public String pass(String text) {
+        return text;
     }
 }
