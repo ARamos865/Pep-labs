f79fd0ee4039bcdb18065c4f8e679f8416c54fa0
Ashley Ramos <ashley.ramos865@my.tccd.edu>
2023-01-05 04:50:09 +0000

    Saved progress on Lists 2023-01-05 04:50:09 +0000

--- UncheckedException/src/main/java/CauseUncheckedException.java
+++ UncheckedException/src/main/java/CauseUncheckedException.java
@@ -1,5 +1,5 @@
 
-public class CauseUncheckedException {
+public class CauseUncheckedException extends RuntimeException {
     /**
      * This method must throw an unchecked exception.
      * unchecked means that you do not need to wrap the method in a try/catch or a throws declaration.
@@ -14,8 +14,12 @@ public class CauseUncheckedException {
      * event happens, like running out of computer memory.
      */
     
-     public void throwUncheckedException()  { 
-       
-    }
+     public void throwUncheckedException()  {
+
+      }
+       public CauseUncheckedException(String message) {
+        super(message);
+       }
+    
         
 }
