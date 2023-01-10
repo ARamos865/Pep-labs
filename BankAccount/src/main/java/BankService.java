9a5b1a2cf5ef6559af0b1f8ffc0abdac8fcbfa3a
Ashley Ramos <ashley.ramos865@my.tccd.edu>
2023-01-10 05:10:47 +0000

    Saved progress on BankAccount 2023-01-10 05:10:47 +0000

--- BankAccount/src/main/java/BankService.java
+++ BankAccount/src/main/java/BankService.java
@@ -23,7 +23,7 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
-
+        this.balance = balance + amount;
     }
 
     /**
@@ -32,7 +32,13 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
+        
+        if(balance >= amount) {
+            balance = balance - amount;
+        }else {
 
+        }
+        
     }
 
     /**
@@ -40,6 +46,6 @@ public class BankService {
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+        return balance;
     }
 }
