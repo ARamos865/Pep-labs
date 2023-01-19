45e6f6f32190655513ff882937c92383d17886a8
Ashley Ramos <ashley.ramos865@my.tccd.edu>
2023-01-19 04:28:33 +0000

    Saved progress on IsSorted 2023-01-19 04:28:33 +0000

--- IsSorted/src/main/java/IsSorted.java
+++ IsSorted/src/main/java/IsSorted.java
@@ -7,6 +7,21 @@ public class IsSorted {
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
     public boolean checkSort(int[] arr){
-        return false;
+       
+       int n = arr.length;
+
+        if (n == 0 || n == 1)
+        return true;
+
+    for (int i = 1; i < n; i++)
+
+       
+        if (arr[i - 1] > arr[i])
+            return false;
+
+    
+    return true;
     }
 }
+
+    
