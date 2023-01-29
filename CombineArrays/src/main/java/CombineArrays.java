7c3f7024bf525bf820d22f8043aa46e0fa6d3d9f
Ashley Ramos <ashley.ramos865@my.tccd.edu>
2023-01-29 05:16:41 +0000

    Saved progress on CombineArrays 2023-01-29 05:16:41 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -8,7 +8,18 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
-
-        return null;
+        int length = arr1.length + arr2.length;
+        int [] mergedArray = new int[length];
+        int pos = 0;
+        for(int element : arr1) {
+            mergedArray[pos] = element;
+            pos++;
+        }
+        for(int element : arr2) {
+            mergedArray[pos] = element;
+            pos++;
+        }
+       
+        return mergedArray;
     }
 }
