7c3b656df35037e74ca63f0ceb70f23134d1a72e
Ashley Ramos <ashley.ramos865@my.tccd.edu>
2023-01-29 04:56:55 +0000

    Saved progress on Search2dArray 2023-01-29 04:56:55 +0000

--- Search2dArray/src/main/java/Search2dArray.java
+++ Search2dArray/src/main/java/Search2dArray.java
@@ -1,3 +1,4 @@
+
 public class Search2dArray {
     /**
      * Receive a 2d array of ints. (That's an array of arrays!)
@@ -10,6 +11,13 @@ public class Search2dArray {
      * @return true if nums contains target, and false otherwise.
      */
     public boolean search(int[][] nums, int target){
+       for(int i = 0; i < nums.length; i++) {
+        for(int j = 0; j <= i; j++) {
+            if(nums[i][j] == target) {
+                return true;
+            }
+        }
+       }
         return false;
     }
 }
