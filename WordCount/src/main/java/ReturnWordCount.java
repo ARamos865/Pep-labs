1592380b7d37021f1964f9b90194015a5d9945f5
Ashley Ramos <ashley.ramos865@my.tccd.edu>
2023-01-31 05:30:03 +0000

    Saved progress on WordCount 2023-01-31 05:30:03 +0000

--- WordCount/src/main/java/ReturnWordCount.java
+++ WordCount/src/main/java/ReturnWordCount.java
@@ -5,6 +5,10 @@ public class ReturnWordCount {
      * @return return the amount of words in a string.
      */
     public int count(String in){
+        if(in == null || in.isEmpty())
         return 0;
+
+        int countWords = in.split(" ").length;
+        return countWords;
     }
 }
